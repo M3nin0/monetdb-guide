2bb6f359b7a1df2cf3e019629aa25f4d02d5a920
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-07-14 23:31:23 -0300

    Remoção de arquivo inválido

--- import-geospatial-data/00-generate-table.sh
+++
@@ -1,4 +0,0 @@
-
-CREATE TABLE uf (gid serial, id float, nome varchar(50), sigla varchar(2), geocodigo varchar(2), regiao varchar(20), regiao_sig varchar(2), geom MULTIPOLYGON);
-
-CREATE TABLE municipios (gid serial, id float, nome varchar(60), geocodigo varchar(7), uf varchar(50), uf_sigla varchar(2), uf_geocodi varchar(2), regiao varchar(20), regiao_sig varchar(2), geom MULTIPOLYGON);
